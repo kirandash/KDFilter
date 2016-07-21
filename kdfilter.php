53d38e74ecba431aa4a1bce5fbd546b6f36dada3
kirandash <thekirankumardash@gmail.com>
2016-07-21 23:01:40 +0530

    4.Final-Project-Additional-Category-Links

--- kdfilter.php
+++ kdfilter.php
@@ -26,4 +26,45 @@ add_action('comment_post', function(){
 		'A new comment has been left on your blog. Please check.'
 	);
 });
+
+add_filter('the_content', function($content){
+	$id = get_the_id();
+	if(!is_singular('post')){
+		return $content;
+	}
+	
+	$terms = get_the_terms($id, 'category');
+	$cats = array();
+	
+	foreach($terms as $term){
+		$cats[] = $term->term_taxonomy_id;
+	}
+	print_r($cats);
+	$loop = new WP_Query(
+		array(
+			'posts_per_page' => 3,
+			'category__in' => $cats,
+			'orderby' => 'rand',
+			'post__not_in' => array($id)
+		)
+	);
+	
+	if($loop->have_posts()) {
+		$content .= '<h2>Related posts</h2>
+					<ul class="related-category-posts">';
+					
+		while($loop->have_posts()){
+			$loop->the_post();
+			$content .= '
+			<li>
+				<a href="'.get_the_permalink().'">'.get_the_title().'</a>
+			</li>
+			';
+		}
+		$content .= '</ul>';
+		wp_reset_query();
+	}
+	
+	return $content;	
+});
 ?>
