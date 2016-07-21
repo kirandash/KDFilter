7e756536796809ce4df676d653f2a1ecab6e11ea
kirandash <thekirankumardash@gmail.com>
2016-07-21 22:29:10 +0530

    3.ActionHooks

--- kdfilter.php
+++ kdfilter.php
@@ -13,4 +13,17 @@ add_filter('the_title',strtoupper);
 add_filter('the_content', function($content){
 	return $content.' By Kiran Dash on '.date('d M Y');
 });
+
+add_action('wp_footer', function(){
+	echo '<p style="color: white;">This content will come before footer.</p>';
+});
+
+add_action('comment_post', function(){
+	$email = get_bloginfo('admin_email');
+	wp_mail(
+		$email,
+		'New comment posted',
+		'A new comment has been left on your blog. Please check.'
+	);
+});
 ?>
